24eef4bac997695ae32cf1fc3087629d3c7ecc69
Sergey <kopgamerk@gmail.com>
2022-03-15 08:50:32 +0300

    No code

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var blueColorView: UIView!
+    
+    @IBOutlet var countOfRed: UILabel!
+    @IBOutlet var countOfGreen: UILabel!
+    @IBOutlet var countOfBlue: UILabel!
+    
+    
+    @IBOutlet var sliderForRed: UISlider!
+    @IBOutlet var sliderForGreen: UISlider!
+    @IBOutlet var sliderForBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColorView.backgroundColor = .red
+        greenColorView.backgroundColor = .green
+        blueColorView.backgroundColor = .blue
+        
+        sliderForRed.minimumTrackTintColor = .red
+        sliderForGreen.minimumTrackTintColor = .green
+        sliderForBlue.minimumTrackTintColor = .blue
+        
     }
 
-
+    @IBAction func sliderForRedSliding() {
+    }
+    
+    @IBAction func sliderForGreenSliding() {
+    }
+    
+    @IBAction func sliderForBlueSliding() {
+    }
+    
+    
+    
 }
 
+extension ViewController {
+    
+}
