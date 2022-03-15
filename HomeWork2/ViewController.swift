aeaa5fc6c26203fa55e7cee2e02fbf05ccb620fb
Sergey <kopgamerk@gmail.com>
2022-03-15 21:22:13 +0300

    Done

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet var countOfRed: UILabel!
-    @IBOutlet var countOfGreen: UILabel!
-    @IBOutlet var countOfBlue: UILabel!
+    @IBOutlet private var countOfRed: UILabel!
+    @IBOutlet private var countOfGreen: UILabel!
+    @IBOutlet private var countOfBlue: UILabel!
     
-    @IBOutlet var currentColorView: UIView!
+    @IBOutlet private var currentColorView: UIView!
     
-    @IBOutlet var sliderForRed: UISlider!
-    @IBOutlet var sliderForGreen: UISlider!
-    @IBOutlet var sliderForBlue: UISlider!
+    @IBOutlet private var sliderForRed: UISlider!
+    @IBOutlet private var sliderForGreen: UISlider!
+    @IBOutlet private var sliderForBlue: UISlider!
+    
+    @IBOutlet private var buttonToGetColor: UIButton!
+    
+    @IBOutlet private var convertedReed: UILabel!
+    @IBOutlet private var convertedGreen: UILabel!
+    @IBOutlet private var convertedBlue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +36,21 @@ class ViewController: UIViewController {
             blue: 1,
             alpha: 1
         )
+        currentColorView.layer.borderWidth = 1.4
+        currentColorView.layer.borderColor = UIColor(red: 0,
+                                                     green: 0,
+                                                     blue: 0,
+                                                     alpha: 1).cgColor
         
         sliderForRed.minimumTrackTintColor = .red
         sliderForGreen.minimumTrackTintColor = .green
         sliderForBlue.minimumTrackTintColor = .blue
         
+        buttonToGetColor.layer.cornerRadius = 15
+        
     }
 
+    
     @IBAction func sliderForRedSliding() {
         setColor()
         let roundedValue = roundf(sliderForRed.value * 100) / 100
@@ -55,6 +69,18 @@ class ViewController: UIViewController {
         countOfBlue.text = String(roundedValue)
     }
     
+    @IBAction func gettingColor() {
+        let color = CIColor(color: currentColorView.backgroundColor!)
+        convertedReed.text =
+        String(roundf(Float(color.red) * 255 * 1) / 1)
+        
+        convertedGreen.text =
+        String(roundf(Float(color.green) * 255 * 1) / 1)
+        
+        convertedBlue.text =
+        String(roundf(Float(color.blue) * 255 * 1) / 1)
+    }
+    
 }
 
 extension ViewController {
@@ -65,5 +91,7 @@ extension ViewController {
             blue: CGFloat(sliderForBlue.value),
             alpha: 1
         )
+        print(CIColor.init(color: currentColorView.backgroundColor!))
     }
 }
+
